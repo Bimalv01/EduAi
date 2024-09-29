ed8d14178ef5b1e9a28b4855c903a21bcefb31dd
bimal <bimalbabu720@gmail.com>
2024-09-29 18:34:01 +0530

    email passwd

--- school_project/settings.py
+++ school_project/settings.py
@@ -148,4 +148,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'bimalbabu720@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'mjcq jobu kzxy emru'
